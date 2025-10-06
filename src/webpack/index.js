d49dd59537e7b5405ab400b46a5e0a958cc7b3ff
sowmiya637 <sowmiyaravichandran637@gmail.com>
2025-10-06 16:25:58 +0530

    Delete src directory

--- src/webpack/index.js
+++
@@ -1,9 +0,0 @@
-import { greet } from './utils.js';
-
-const message = greet("Sowmiya");
-console.log(message);
-
-// Display in browser
-const h1 = document.createElement("h1");
-h1.textContent = message;
-document.body.appendChild(h1);
